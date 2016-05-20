ad49ff7264cec86e965df15875a18ab28184984b
petemill <miller.pete+commit@gmail.com>
2016-05-19 19:39:16 -0700

    ngrok not neccessary, that's what finesse does

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const logger = require('./logger');
-const ngrok = require('ngrok');
 
 const frontend = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
@@ -26,17 +25,5 @@ app.listen(port, (err) => {
   if (err) {
     return logger.error(err);
   }
-
-  // Connect to ngrok in dev mode
-  if (isDev) {
-    ngrok.connect(port, (innerErr, url) => {
-      if (innerErr) {
-        return logger.error(innerErr);
-      }
-
-      logger.appStarted(port, url);
-    });
-  } else {
-    logger.appStarted(port);
-  }
+  logger.appStarted(port);
 });
